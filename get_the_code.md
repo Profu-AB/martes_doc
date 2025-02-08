79059ddd77cd29e1db2b989a89fc8cbcd2a1cd81
Martin <martin.magnemyr@gmail.com>
2025-02-08 08:22:24 +0100

    added setupgit.sh

--- get_the_code.md
+++ get_the_code.md
@@ -1,5 +1,6 @@
 # martes git repo
-this is a private repo so you will need to request access to it
+- this is a private repo so you will need to request access to it
+- read setupgit.md 
 
 # clone the repo
 git clone https://github.com/mmagnemyr/martes.git
--- setupgit.sh
+++ setupgit.sh
@@ -1,7 +1,24 @@
-git config --global user.email "martin.magnemyr@gmail.com"
-git config --global user.name "Martin Magnemyr"
+sudo apt-get install xclip   # for Ubuntu/Debian-based systems
+sudo apt-get install xsel    # for Ubuntu/Debian-based systems
 
-ssh-keygen -t rsa -b 4096 -C "martin.magnemyr@gmail.com"
+
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
+# Set Git config
+git config --global user.email "$GIT_USER_EMAIL"
+git config --global user.name "$GIT_USER_NAME"
+
+# Generate SSH key
+ssh-keygen -t rsa -b 4096 -C "$GIT_USER_EMAIL"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub
+# cat ~/.ssh/id_rsa.pub
+
+cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
+
+echo go to https://github.com/settings/keys
+echo then run scri
